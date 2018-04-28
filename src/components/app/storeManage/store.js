39e1048689ed0d68e411a087b159100b30688cad
hemei1212 <627100403@qq.com>
2018-04-28 16:06:21 +0800

    storeManage

--- src/components/app/storeManage/store.js
+++ src/components/app/storeManage/store.js
@@ -1,14 +1,91 @@
+import axios from "axios"
 export default {
     namespaced: true,
     state: {
-       
-    },
-    mutations: {
+        tableData5: [      //列表的信息
 
+        ],
+        imgList:[],
+     
+        obj:{
+            shopLicenceNum: '营业执照号码',   //营业执照号码
+            shopLicenceImg: '',//营业执照图片
+            shopAdd: '',//营业地址
+            shopCorporate: '',//法人
+            shopTel: '', //联系电话
+            shopImg: '',//头图
+            shopFeature: '',//特色
+            shopEmployee: '',//店员属性
+            shopDes:'',   //店铺的描述
+        },
+        isList: true,
+        isAdd:true,
+        labelPosition: 'right',
+        
+        formLabelAlign: {       //增加的信息
+              shopName: '',
+            manageId: '5ae17ec646f20cde65de9f69',
+              shopLicenceNum: '',   //营业执照号码
+              shopLicenceImg: '',//营业执照图片
+              shopAdd: '',//营业地址
+              shopCorporate: '',//法人
+              shopTel: '', //联系电话
+              shopImg: '',//头图
+              shopFeature: '',//特色
+              shopEmployee: '',//店员属性
+              shopDes:'',
+            imageUrl: '',
+
+        },
+        newDataId:{
+            dataId:"",
+            whatImg:"shopImg"  
+        },
+      
+     
+    },
+    mutations: {         //方法
+        getshops(state, payload) {
+            // console.log(payload.rows);
+            state.tableData5 = payload.rows
+        },
+        getIsList(state){    //点击修改后跳转页面，取反
+            state.isList=!state.isList
+        },
+        searchData(){
+
+        }
+  
     },
     actions: {
-        async getMsg(context) {
+        async postMsg(context, storeDate) {    //增加            
+            const { data } = await axios.post("/storeManage", context.state.formLabelAlign)   //请求跨域，并且传了一个参数storeDat
+            context.state.newDataId.dataId = data._id;
+            context.state.isAdd = false;
+            await context.dispatch("getMsg")  //触发 获取的
+        },
+        async getMsg(context) {    //获取
+            const res = await axios.get("/storeManage")   //请求跨域
+  
+            context.commit("getshops", res.data)
+        },
+        async delMsg(context, id) {    //删除
+            // console.log(111,id)
+            await axios.delete("/storeManage/" + id)   //请求跨域
+        },
+        async changeMsg(context,item) {    //修改
+            const { id,manageId, shopName, shopEmployee, shopLicenceNum, shopAdd, shopCorporate, shopTel, shopFeature, shopDes } = item
+            await axios.put("/storeManage/" + id,item)   //请求跨域
+        },
+        async searchMsg(context,valueData) {    //搜索
+            console.log(222,valueData)    //get传参数，后面不能用逗号加参数，post可以，必须是？后加参数
+            await axios.get(`/storeManage/?type=shopName&value=${valueData}`).then(function(res){
+                console.log(999,res.data)
+                context.state.tableData5=res.data;
+            })
+          
            
-        }
+        },
+
     }
 }
--- src/components/app/storeManage/storeAdd.vue
+++ src/components/app/storeManage/storeAdd.vue
@@ -1,5 +1,151 @@
+
 <template>
-  <h1>
-      新增
-  </h1>
+ <div>
+   <!-- <h1>新增列表</h1> -->
+  <div v-if="isAdd">
+    <el-form :label-position="labelPosition" ref="ruleForm2" label-width="100px" :model="formLabelAlign">
+  
+      <el-form-item label="店主ID">
+        <el-input v-model="formLabelAlign.manageId" style="width: 400px;" :disabled="true"></el-input>
+      </el-form-item>
+      <el-form-item label="门店名称">
+        <el-input v-model="formLabelAlign.shopName" style="width: 400px;"></el-input>
+      </el-form-item>    
+      <el-form-item label="营业执照号码">
+        <el-input v-model="formLabelAlign.shopLicenceNum" style="width:400px;"></el-input>
+      </el-form-item>
+      <el-form-item label="营业地址">
+        <el-input v-model="formLabelAlign.shopAdd" style="width: 400px;"></el-input>
+      </el-form-item>
+
+      <el-form-item label="法人">
+        <el-input v-model="formLabelAlign.shopCorporate" style="width: 400px;"></el-input>
+      </el-form-item>
+      <el-form-item label="联系电话">
+        <el-input v-model="formLabelAlign.shopTel" style="width: 400px;"></el-input>
+      </el-form-item>
+
+      <el-form-item label="特色">
+        <el-input v-model="formLabelAlign.shopFeature" style="width: 400px;"></el-input>
+      </el-form-item>
+      <el-form-item label="店员属性">
+        <el-input v-model="formLabelAlign.shopEmployee" style="width: 400px;"></el-input>
+      </el-form-item>
+        <el-form-item label="店铺的描述">
+        <el-input v-model="formLabelAlign.shopDes" style="width: 400px;"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button  @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <div v-else>
+    <el-upload
+      class="upload-demo"
+      action="/storeManage/upload"
+      :data = "newDataId"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :file-list="imgList"
+      list-type="picture">
+       <div style="margin-bottom:20px;width:120px" >
+        <el-select v-model="newDataId.whatImg"  placeholder="请选择">
+          <el-option value="shopLicenceImg" label="营业执照"></el-option>
+          <el-option value="shopImg" label="店铺头图"></el-option>
+        </el-select>    
+      </div>
+      <el-button size="small" type="primary">点击上传</el-button>
+     
+           
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+    </el-upload>
+        <el-button type="primary" style="margin-top:20px;" @click="submitFanhui()">返回用户列表</el-button>
+  </div>
+ 
+</div>
+  
 </template>
+<script>
+import { mapState, mapActions, mapMutations } from "vuex";
+import storeListVue from './storeList.vue';
+export default {
+  // data() {
+  // return {
+  // labelPosition: 'right',
+  // formLabelAlign: {
+  //   shopName: '',
+  //   manageId: '',
+  //   shopLicenceNum: '',   //营业执照号码
+  //   shopLicenceImg: '',//营业执照图片
+  //   shopAdd: '',//营业地址
+  //   shopCorporate: '',//法人
+  //   shopTel: '', //联系电话
+  //   shopTel: '',//头图
+  //   shopFeature: '',//特色
+  //   shopEmployee: '',//店员属性
+  //   shopDes:''
+
+  // }
+  // };
+  // },
+  computed: {
+    ...mapState("storeManage", [
+      "labelPosition",
+      "formLabelAlign",
+      "isAdd",
+      "imgList",
+      "newDataId"
+    ]),
+
+  },
+  methods: {
+    ...mapActions("storeManage", ["postMsg","getMsg"]),
+    submitForm(formName) {  //提交
+      this.postMsg();   //控制新增的某个列表的出现
+    },
+    submitFanhui(){  //返回到用户列表
+      this.$router.push({path:'/info/storeManage/storeList'})
+    },
+    resetForm(formName) {
+      //重置
+      this.$refs[formName].resetFields();
+    },
+    handleRemove(file, fileList) {
+      // console.log(file, fileList);
+    },
+    handlePreview(file) {
+      // console.log(file);
+    }
+  }
+};
+</script>
+
+
+
+ <style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
