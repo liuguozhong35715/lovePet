990e38a604250f0e8b1a8d231f5971186a0bba18
Adele2018 <495132717@qq.com>
2018-06-18 18:06:14 +0800

    测试分支

--- src/components/store/test.js
+++ src/components/store/test.js
@@ -12,11 +12,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        commodity,
         customer,
+        commodity,
         order,
-        pet,
         server,
+        pet,
         storeManage,
         user
     }
